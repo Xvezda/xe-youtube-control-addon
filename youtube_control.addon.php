d802fcdc279b855b4ad7e7d0be714d31ebdfd8ad
Xvezda <xvezda@naver.com>
2014-11-02 01:21:21 +0900

    change indent space to tab

--- youtube_control.addon.php
+++ youtube_control.addon.php
@@ -13,7 +13,7 @@ if(!defined('__XE__'))
  */
 if($called_position == 'before_display_content' && Context::get('module') != 'admin' && $oModule->act == "dispBoardContent" && (Context::getResponseMethod() == 'HTML' || !isCrawler()))
 {
-    $tags = <<<EOD
+	$tags = <<<EOD
 <script>
 //<![CDATA[
 var youtube_resize = %s;
@@ -23,26 +23,26 @@ var youtube_query = {
 //]]>
 </script>
 EOD;
-    
-    $resize = ($addon_info->resize != '') ? 'true' : 'false';
-    
-    $temp_arr = array(); // temp array
-    $func_arr = array('xe_validator_id', 'xe_run_method', 'resize');
-    
-    foreach($addon_info as $key => $val)
-    {
-        if(in_array($key, $func_arr) || $val == '') continue;
+
+	$resize = ($addon_info->resize != '') ? 'true' : 'false';
+
+	$temp_arr = array(); // temp array
+	$func_arr = array('xe_validator_id', 'xe_run_method', 'resize');
+
+	foreach($addon_info as $key => $val)
+	{
+		if(in_array($key, $func_arr) || $val == '') continue;
         
-        array_push($temp_arr, '    "'.$key.'" : "'.$val.'"'); // Push
-    }
-    $params = implode($temp_arr, ','.PHP_EOL); // Array to String
-    
-    $tags = sprintf($tags, $resize
-                    , $params);
-    
-    Context::addHtmlFooter($tags);
-    
-    Context::loadFile(array('./addons/youtube_control/js/youtube_control.min.js', 'body', '', null), true);
+		array_push($temp_arr, '    "'.$key.'" : "'.$val.'"'); // Push
+	}
+	$params = implode($temp_arr, ','.PHP_EOL); // Array to String
+
+	$tags = sprintf($tags, $resize
+					, $params);
+
+	Context::addHtmlFooter($tags);
+
+	Context::loadFile(array('./addons/youtube_control/js/youtube_control.min.js', 'body', '', null), true);
 }
 
 /* End of file youtube_control.addon.php */
