8b0f7c39f3233528f727feb7f4b467252672a45f
Xvezda <xvezda@naver.com>
2014-11-05 10:23:21 +0900

    indent fixed

--- youtube_control.addon.php
+++ youtube_control.addon.php
@@ -24,7 +24,7 @@ var youtube_option = {
 </script>
 EOD;
 
-    $resize = ($addon_info->resize_force != '' && preg_match("/^\d+$/", $addon_info->resize_force)) ? $addon_info->resize_force : (($addon_info->resize != '') ? $addon_info->resize : 'off');
+	$resize = ($addon_info->resize_force != '' && preg_match("/^\d+$/", $addon_info->resize_force)) ? $addon_info->resize_force : (($addon_info->resize != '') ? $addon_info->resize : 'off');
 
 	$temp_arr = array(); // temp array
 	$func_arr = array('xe_validator_id', 'xe_run_method', 'mid_list', 'resize', 'resize_force');
